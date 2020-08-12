4a8c957de1c0d9af6cfc371e81d75849bc802f2d
Austin Lothenbach <austin.lothenbach@smail.rasmussen.edu>
2020-08-11 21:24:27 -0500

    Updates

---
+++ precache-manifest.28af24e863852a15876834666a88cf82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31d020ae589bd77176b14c1f8b3713d",
+    "url": "/WebFrameworks/index.html"
+  },
+  {
+    "revision": "776bc93e86670bd765ea",
+    "url": "/WebFrameworks/static/css/main.e6e4ea98.chunk.css"
+  },
+  {
+    "revision": "aa3919c409104e3d7107",
+    "url": "/WebFrameworks/static/js/2.91b9cb10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WebFrameworks/static/js/2.91b9cb10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776bc93e86670bd765ea",
+    "url": "/WebFrameworks/static/js/main.fe19fc7d.chunk.js"
+  },
+  {
+    "revision": "808fd5bcc9126acf67ad",
+    "url": "/WebFrameworks/static/js/runtime-main.19ab9004.js"
+  }
+]);
